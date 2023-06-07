acfd04ab00c09edbd658b4d2fea0598463a15646
Domingos Canhanga <canhanga96@gmail.com>
2023-06-07 10:11:59 +0100

    🚀 finish all section with content

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import './../styles/index.css';
+const year = document.querySelector('#year')!;
+year.innerHTML = new Date().getFullYear().toString();
